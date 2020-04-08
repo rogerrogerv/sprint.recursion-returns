43294dcdfc43463e8151662e15383bcca74bbe3c
CarlosIUSalazar <carlosiusalazar@gmail.com>
2020-04-08 16:41:04 +0900

    getElementsByClassName complete
    Co-authored-by: rogerrogerv <rogerrogerv@users.noreply.github.com>
    Co-authored-by: carlosiusalazar <carlosiusalazar@users.noreply.github.com>

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -1,5 +1,50 @@
-const getElementsByClassName = () => {
+/* eslint-disable prefer-const */
+/* eslint-disable prettier/prettier */
+const getElementsByClassName = (className, node) => {
   // YOUR CODE HERE
+
+  let results = [];
+
+  if (!node) {
+    node = document.body;
+  } else {
+    node;
+  }
+
+  if (node.classList && node.classList.contains(className)) {
+    results.push(node);
+    //console.log('AAAAAAAAA',node);
+  }
+
+  let children = node.childNodes;
+  if (children) {
+    for (let child of children) {
+      results = results.concat(getElementsByClassName(className, child));
+      //console.log('BBBBBBBBBB',results)
+    }
+  }
+  return results;
 };
 
 module.exports = { getElementsByClassName };
+
+// const getElementsByClassName = (className) => {
+//   let output = []
+//   let dom = document.documentElement
+
+//   let findClass = function(node){
+//     if (node.classList && node.classList.contains(className)){
+//       output.push(node)
+//     }
+//     if (node.childNodes){
+//       node.childNodes.forEach((element) => {
+//         findClass(element)
+//       })
+//     }
+//   }
+
+//   findClass(dom)
+//   return output
+// };
+
+// module.exports = { getElementsByClassName };
--- src/stringifyJSON.js
+++
@@ -1,8 +0,0 @@
-const stringifyJSON = () => {
-  // YOUR CODE HERE
-};
-
-module.exports = { stringifyJSON };
-
-
-https://rogerrogerv@github.com/rogerrogerv/cc12-sprint.recursion-returns.git
